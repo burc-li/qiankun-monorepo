1325467f4c0ca6f927d7775579f5bcedace7ae39
libaicheng <1477996221@qq.com>
2024-10-28 17:52:22 +0800

    feat: 初始化主应用

---
+++ main/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <div class="app">
+    <Header></Header>
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.app {
+  // width: 100vw;
+  // height: 100vh;
+  .content{
+    padding: 14px 16px;
+    // height: calc(100% - @nav-height);
+    overflow: auto;
+  }
+}
+</style>
