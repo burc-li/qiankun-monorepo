6901e225852a02bd7f33ac794dd62ed78b767969
libaicheng <libaicheng@qq.com>
2024-11-02 20:54:50 +0800

    feat: 新增monorepo共享包案例

--- microApps/vue2-map-drilling/src/views/consumer/index.vue
+++ microApps/vue2-map-drilling/src/views/consumer/index.vue
@@ -2,11 +2,16 @@
   <div class="consumer-wrapper">
     <section class="consumer-item">
       <h3>monorepo共享包（@libc/utils）</h3>
-      <div class="consumer-content">124</div>
+      <div class="consumer-content">
+        <p>@libc/utils 中的 array2Object 方法</p>
+        {{array2Object}}
+        </div>
     </section>
     <section class="consumer-item">
       <h3>monorepo共享包（@libc/desgin3）</h3>
-      <div class="consumer-content">124</div>
+      <div class="consumer-content">
+        <p>@libc/desgin3 组件库仅支持 vue3，<a :href="link">跳转</a> 至 vue3 项目查看</p>
+      </div>
     </section>
     <section class="consumer-item">
       <h3>模块联邦（@libc/desgin3）</h3>
@@ -20,9 +25,21 @@
 </template>
 
 <script>
+import { array2Object } from '@libc/utils'
 export default {
   name: 'Consumer',
-  components: {
+  data () {
+    return {
+      array2Object,
+      title: `${window.location.origin}`,
+    }
+  },
+  computed: {
+    link () {
+      return `${window.location.origin}/pdf/jspdf`
+    },
+  },
+  mounted () {
   },
 }
 </script>
@@ -41,8 +58,18 @@ export default {
     width: 45%;
     height: 40%;
     margin-top: 30px;
-    border: 2px solid #de5e60;
+    padding: 16px;
     border-radius: 4px;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+    h3{
+      font-size: 16px;
+      color: #4fc084;
+      font-weight: bold;
+      margin-bottom: 22px;
+    }
+    p{
+      margin-bottom: 16px;
+    }
   }
 }
 </style>
