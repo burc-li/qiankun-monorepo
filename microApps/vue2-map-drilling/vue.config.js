380c07c040bbe3a2573ac8c088d1459b07935b80
libaicheng <libaicheng@qq.com>
2024-11-02 12:29:56 +0800

    build: 更新依赖

--- microApps/vue2-map-drilling/vue.config.js
+++ microApps/vue2-map-drilling/vue.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
   // 我们在 public-path.js 修改了 运行时 publicPath，这里是可以省略的
   publicPath: process.env.NODE_ENV ? '/' : '/micro-apps/vue2-map-drilling/',
+
   devServer: {
     // port: 8002,
     port: process.env.VUE_APP_POST,
