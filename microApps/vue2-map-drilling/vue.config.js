00be0c7a8a37195a786f23148f45c2358cb3fd09
libaicheng <libaicheng@qq.com>
2024-11-03 13:30:52 +0800

    style: 全局导入less变量

--- microApps/vue2-map-drilling/vue.config.js
+++ microApps/vue2-map-drilling/vue.config.js
@@ -1,5 +1,6 @@
 const packageName = require('./package.json').name
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -24,9 +25,26 @@ module.exports = defineConfig({
     loaderOptions: {
       less: {
         lessOptions: {
+          modifyVars: {
+            hack: `true;@import (reference)"${path.resolve('src/assets/styles/variable.less')}"`,
+          },
           javascriptEnabled: true,
         },
       },
     },
+  },
+  chainWebpack: config => {
+    // 模块联邦
+    config
+      .plugin('module-feaderation-plugin')
+      .use(require('webpack').container.ModuleFederationPlugin, [
+        {
+          name: 'consumer',
+          // 作为消费方，使用的时通过 ${remoteEntry}/${expose} 的方式使用
+          remotes: {
+            remoteEntry: 'provider@http://localhost:9000/remoteEntry.js',
+          },
+        },
+      ])
   },
 })
